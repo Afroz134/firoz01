cc8b3d5aa85b1c5fe69717b302e63fd42f35eb03
Afroz134 <135991116+Afroz134@users.noreply.github.com>
2023-06-08 20:08:11 +0530

    Add files via upload

---
+++ pdgf.py
@@ -0,0 +1,74 @@
+#print("hhhh")
+#print("hello")
+#a=10
+#if a%2==0:
+
+    #print("code successful")
+
+#else: 
+   # print("code not succesful")
+#n=eval(input('enter the value'))
+#print("hello world")
+#x=5
+#print("hello world",x)
+#print(type(4.0))
+#fruits = ["apple","banana","mango"]
+#x,y,z=fruits
+#print(x)
+#print(y)
+#print(z)
+#x= 5
+#y = "hello world"
+#print(type(y))
+#y = 1j
+#y = ["apple","banana","mango"]
+#y = ("apple","banana","mango")
+#y = {"apple","banana","mango"}
+#y = range(4)
+#y = {"name":"afroz","age":34}
+#y = False
+#x = "hello"
+#print(x[2:3])
+a = "hello"
+print(a.count('l'))
+print(a.capitalize())
+print(a.isalnum())
+print(a.isalpha())
+print(a.isdigit())
+print(a.lower())
+print(a.upper())
+print(a.title())
+print(a.swapcase())
+a = 'work is workship'
+b = a.split()
+print(b)
+x = "my name is afroz and i am {}"
+age = 45
+print(x.format(age))
+b = 100
+m = 45
+print(b == m)
+print(b > m)
+print(b < m)
+print(b+m)
+print(b-m)
+print(b*m)
+print(b/m)
+print(b%m)
+print(b**m)
+print(b//m)
+print(b==m)
+a = 8
+a >>= 7
+print(a)
+n = 23
+c = 10
+if n > c:
+   print("n greater than c")
+else:
+  print("n is a greates number")
+
+
+
+
+
